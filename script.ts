dbfce73e79ffbae0635cf836847155f536c10af0
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-27 08:27:03 -0300

    update

--- script.ts
+++ script.ts
@@ -1,30 +1,36 @@
-var button = document.querySelector('.btn-generate');
-var image = document.querySelector('img');
-var download = document.querySelector('.btn-download');
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+let button = document.querySelector('.btn-generate');
+let image = document.querySelector('img');
+let download = document.querySelector('.btn-download');
+
+button?.addEventListener('click', () => {
     //using the unsplash api
     fetch('https://api.unsplash.com/photos/random?query=dog&client_id=3ElZfutFs2Xro49kzTn3uQiARTF1DTQLfJxPL5lH_mk')
-        .then(function (value) {
-        //converting the result to json
-        return value.json();
-    })
-        .then(function (json) {
-        var dogo = json;
-        console.log(dogo);
-        document.querySelector('span').style.display = 'none';
-        image.src = dogo.urls.regular;
-    })["catch"](function (error) {
-        console.log(error);
-    });
-});
+        .then(value => {
+            //converting the result to json
+            return value.json();
+        })
+            .then(function(json) {
+                let dogo = json;
+                console.log(dogo);
+                document.querySelector('span')!.style.display = 'none';
+                image!.src = dogo.urls.regular;
+            })
+        .catch(error => {
+            console.log(error);
+        })
+})
+
 //To be continued...
+
 // download.addEventListener('click', () => {
+    
 //     if(image.src == '' || image == null){
 //         alert('No image availabe');
 //     } else {
 //         fetchFile(image.src);
 //     }
 // })
+
 // function fetchFile(url) {
 //     fetch(url)
 //         .then(value => {
