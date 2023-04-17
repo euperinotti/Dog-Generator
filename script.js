2e2785d859178d4e39fc383fdd2fc2c9d2c3d234
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-04-17 14:11:45 -0300

    update

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ var button = document.querySelector('.btn-generate');
 var image = document.querySelector('img');
 var download = document.querySelector('.btn-download');
 button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    //using the unsplash api
     fetch('https://api.unsplash.com/photos/random?query=dog&client_id=3ElZfutFs2Xro49kzTn3uQiARTF1DTQLfJxPL5lH_mk')
         .then(function (value) {
-        //converting the result to json
         return value.json();
     })
         .then(function (json) {
@@ -17,26 +15,3 @@ button === null || button === void 0 ? void 0 : button.addEventListener('click',
         console.log(error);
     });
 });
-//To be continued...
-// download.addEventListener('click', () => {
-//     if(image.src == '' || image == null){
-//         alert('No image availabe');
-//     } else {
-//         fetchFile(image.src);
-//     }
-// })
-// function fetchFile(url) {
-//     fetch(url)
-//         .then(value => {
-//             return value.blob();
-//         })
-//             .then(file => {
-//                 let tempUrl = URL.createObjectURL(file);
-//                 let aTag = document.createElement("a");
-//                 aTag.href = tempUrl;
-//                 aTag.download = "dogo";
-//                 document.body.appendChild(aTag);
-//                 aTag.click();
-//                 aTag.remove();
-//             })
-// }
--- script.ts
+++ script.ts
@@ -6,7 +6,6 @@ button?.addEventListener('click', () => {
     //using the unsplash api
     fetch('https://api.unsplash.com/photos/random?query=dog&client_id=3ElZfutFs2Xro49kzTn3uQiARTF1DTQLfJxPL5lH_mk')
         .then(value => {
-            //converting the result to json
             return value.json();
         })
             .then(function(json) {
@@ -19,30 +18,3 @@ button?.addEventListener('click', () => {
             console.log(error);
         })
 })
-
-//To be continued...
-
-// download.addEventListener('click', () => {
-    
-//     if(image.src == '' || image == null){
-//         alert('No image availabe');
-//     } else {
-//         fetchFile(image.src);
-//     }
-// })
-
-// function fetchFile(url) {
-//     fetch(url)
-//         .then(value => {
-//             return value.blob();
-//         })
-//             .then(file => {
-//                 let tempUrl = URL.createObjectURL(file);
-//                 let aTag = document.createElement("a");
-//                 aTag.href = tempUrl;
-//                 aTag.download = "dogo";
-//                 document.body.appendChild(aTag);
-//                 aTag.click();
-//                 aTag.remove();
-//             })
-// }
