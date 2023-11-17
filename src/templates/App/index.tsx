655fed41175c4e76c4d380ccd10cea53e39bfb8c
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-11-17 13:56:40 -0300

    refactor: code refactor

--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,16 +1,23 @@
+import api from '@/axios/axios-provider'
 import { GenerateButton } from '@/components/GenerateButton'
-import * as S from './styles'
 import { ImageArea } from '@/components/ImageArea'
+import { Image } from '@/models/Image'
+import { useState } from 'react'
+import * as S from './styles'
 
 export const AppTemplate = () => {
-  const handleClick = () => {
-    console.log('clicked')
+  const [imageInfo, setImageInfo] = useState<Image>()
+
+  const handleClick = async () => {
+    const image = await api.getDog()
+
+    setImageInfo(image as Image)
   }
 
   return (
     <S.Container>
       <S.Main>
-        <ImageArea src="" />
+        <ImageArea imageInfo={imageInfo as Image} />
         <GenerateButton value="Generate" onClick={handleClick} />
       </S.Main>
     </S.Container>
